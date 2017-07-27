5693b6b62fc23c52e18adfa7d79796428e43894e
First Test_app1 <FirstTest_app1@gmail.com>
2017-07-27 05:44:42 +0530

    Add partials and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,74 +1,51 @@
-  class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+ class ArticlesController < ApplicationController
 
-  # GET /articles
-  # GET /articles.json
-  def index
-    @articles = Article.all
-  end
+   def index
+     @articles  =Article.all
+   end
 
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
-  end
+   def new
+   @article =Article.new
+   end
 
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
+ def edit
+   @article =Article.find(params[:id])
+ end
 
-  # GET /articles/1/edit
-  def edit
-  end
+ def create
+     @article=Article.new(article_params)
+     if @article.save
+       flash[:notice] ="Article was successfully created"
+       redirect_to  article_path(@article)
+     else
+       render 'new'
+       end
 
-  # POST /articles
-  # POST /articles.json
-  def create
-    @article = Article.new(article_params)
+   end
+ def update
+   @article = Article.find(params[:id])
+   if @article.update(article_params)
+     flash[:notice]="Article was succesfully updated"
+     redirect_to article_path(@article)
+   else
+     render 'edit'
+   end
+ end
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   def show
+     @article =Article.find(params[:id])
+   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   def destroy
+     @article = Article.find(params[:id])
+     @article.destroy
+     flash[:notice]="Article was succesfully deleted"
+     redirect_to articles_path
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
-  def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+   end
+   private
+   def article_params
+     params.require(:article).permit(:title,:description)
+   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
-end
+ end
