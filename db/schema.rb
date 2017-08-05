115702a9c41cc84c6ba5299246805917b5299aec
First Test_app1 <FirstTest_app1@gmail.com>
2017-08-05 11:55:30 +0530

    create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802151624) do
+ActiveRecord::Schema.define(version: 20170805051401) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
