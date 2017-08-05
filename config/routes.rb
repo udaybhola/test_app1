b29077ec948570ce9d999ceee0c6125ea3788d5b
First Test_app1 <FirstTest_app1@gmail.com>
2017-08-06 02:50:37 +0530

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'about', to:  'welcome#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'signup' , to: 'users#new'
+  resources :users,except: [:new]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
